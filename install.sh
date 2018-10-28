0effca4692ed8ee30703466381e88b43b9e7dc36
robreder <r.reder@gmx.at>
2018-10-28 11:29:23 +0100

    added reboot to crontab

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 sudo apt-get update
 apt install python3-espeak
+
+path2RaspySay="/path/to/RaspySayIp4Addr/"
+line="@reboot sleep 30 && cd $path2RaspySay && $path2RaspySay/run.sh"
+(crontab -u pi -l; echo "$line" ) | crontab -u pi -
