3e63f3261be938abeb2912e698d63d672ba054a3
robreder <r.reder@gmx.at>
2018-10-28 11:41:45 +0100

    added path

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 sudo apt-get update
-apt install python3-espeak
+sudo apt install -y python3-espeak
 
-path2RaspySay="/path/to/RaspySayIp4Addr/"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+path2RaspySay="/home/pi/RaspySayIp4Addr/"
+
+mkdir $path2RaspySay
+cp -r $DIR $path2RaspySay/*
+
 line="@reboot sleep 30 && cd $path2RaspySay && $path2RaspySay/run.sh"
 (crontab -u pi -l; echo "$line" ) | crontab -u pi -
