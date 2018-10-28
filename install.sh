ac2f53a0b560c8e08787480d6819bb1830316236
robreder <r.reder@gmx.at>
2018-10-28 15:25:21 +0100

    changed install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 path2RaspySay="/home/pi/RaspySayIp4Addr/"
 
 mkdir $path2RaspySay
-cp -r $DIR $path2RaspySay/
+cp -r $DIR/* $path2RaspySay
 
 line="@reboot sleep 30 && cd $path2RaspySay && $path2RaspySay/run.sh"
 (crontab -u pi -l; echo "$line" ) | crontab -u pi -
