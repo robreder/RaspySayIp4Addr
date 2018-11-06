96b01038e99762c66c299229b254b249fa71bef3
robreder <r.reder@gmx.at>
2018-11-06 13:36:33 +0100

    Install script has got some comments and app folder will be hidden now.

--- install.sh
+++ install.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
+# This script will install the application for user "pi".
+# If you have a different user change the user name!
+RASPI_USER=pi
+
+# install relevant python3 libraries
 sudo apt-get update
 sudo apt install -y python3-espeak
 sudo apt install -y python3-netifaces
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-path2RaspySay="/home/pi/RaspySayIp4Addr/"
+# remember current working directory
+ORIG_DIR="$( pwd )"
 
+# change to installation directory 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+path2RaspySay="/home/${RASPI_USER}/.raspySayIp4Addr/"
+
+# copy the application into the user folder
 mkdir $path2RaspySay
 cp -r $DIR/* $path2RaspySay
 
+# set automatic program call on system boot
 line="@reboot sleep 30 && cd $path2RaspySay && ${path2RaspySay}run.sh"
-(crontab -u pi -l; echo "$line" ) | crontab -u pi -
+(crontab -u $RASPI_USER -l; echo "$line" ) | crontab -u $RASPI_USER -
+
+# change back to initial working directory
+cd $ORIG_DIR
