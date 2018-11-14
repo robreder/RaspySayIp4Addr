8fbc5992de5d8ce821747323f9daeb83e930c6d0
robreder <r.reder@gmx.at>
2018-11-14 08:45:51 +0100

    added audio channel handling in run-script
    audio channel handling for raspberry pi.

--- run.sh
+++ run.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
+
+# remember original audio output channel
+origAudChan=`amixer cset numid=3 | grep ": values=" | cut -d= -f2`
+analogChan=1
+echo "Original channel is $origAudChan ."
+# set audio output to analog
+amixer cset numid=3 $analogChan
+
+# start IP output on analog audio channel
 python3 sayIp4Addr.py 2>/dev/null
+
+# set back original audio output channel
+amixer cset numid=3 $origAudChan
