58c9e5e9bd75feb522e68ebb1ff808a959728f01
robreder <r.reder@gmx.at>
2018-11-14 10:04:16 +0100

    added deinstallation file

---
+++ deinstall.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+cd ~
+path2RaspySay="/home/${RASPI_USER}/.raspySayIp4Addr/"
+
+
+
+# delete crontab line of script
+line=`(crontab -u pi -l | grep $path2RaspySay)`
+echo -n "Removing line '${line}' from automatic cronjobs."
+(crontab -u pi -l | grep -v $path2RaspySay ) | crontab -u pi -
+echo " - done"
+echo "-------"
+
+#mkdir $path2RaspySay
+echo "!! Are you sure you want to delete following folder to deinstall this app?"
+echo "You can remove this folder later."
+rm -r -I $path2RaspySay
+echo "Deinstallation completed!"
