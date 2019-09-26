08aecf2b0f1a9c5472b12671e95c4185edb9c900
robreder <r.reder@gmx.at>
2019-09-26 17:42:09 +0200

    Try workaround without espeak of python. Instead using espeak with omxplayer over os command.

--- sayIp4Addr.py
+++ sayIp4Addr.py
@@ -6,6 +6,7 @@ __version__ = "1.0"
 
 from time import sleep
 from espeak import espeak  # apt install python3-espeak
+import os
 
 def sayChar(word):
     ''' say a number word or dot '''
@@ -23,8 +24,18 @@ def sayChar(word):
 
 def sayWord(word):
     if isinstance(word, str):
-        for c in word:
-            sayChar(c)
+        if word == 'next':
+            myCmd = 'echo "next IP 4 address" | espeak --stdin -wtest.wav && omxplayer test.wav > /dev/null && rm test.wav && sleep 1'
+            #print(myCmd)
+            os.system(myCmd)
+        else:
+            for c in word.split('.'):
+                print(c)
+                myCmd = 'echo "' + c +  '" | espeak --stdin -wtest.wav && omxplayer test.wav > /dev/null && rm test.wav'
+                #print(myCmd)
+                os.system(myCmd)
+        #for c in word:
+        #    sayChar(c)
 
 try:
     
