5f008f8023bdccb299ed19cf6b17946c73fb3138
robreder <r.reder@gmx.at>
2025-03-10 16:39:31 +0100

    Update sayIp4Addr.py using cvlc instead of omxplayer

--- sayIp4Addr.py
+++ sayIp4Addr.py
@@ -25,13 +25,15 @@ def sayChar(word):
 def sayWord(word):
     if isinstance(word, str):
         if word == 'next':
-            myCmd = 'echo "next IP 4 address" | espeak --stdin -wtest.wav && omxplayer -o local test.wav > /dev/null && rm test.wav && sleep 1'
+            myCmd = 'echo "next IP 4 address" | espeak --stdin -wtest.wav && cvlc --play-and-exit test.wav > /dev/null && rm test.wav && sleep 1'
+            #myCmd = 'echo "next IP 4 address" | espeak --stdin -wtest.wav && omxplayer -o local test.wav > /dev/null && rm test.wav && sleep 1'
             #print(myCmd)
             os.system(myCmd)
         else:
             for c in word.split('.'):
                 print(c)
-                myCmd = 'echo "' + c +  '" | espeak --stdin -wtest.wav && omxplayer -o local test.wav > /dev/null && rm test.wav'
+                myCmd = 'echo "' + c +  '" | espeak --stdin -wtest.wav && cvlc --play-and-exit test.wav > /dev/null && rm test.wav && sleep 0.5'
+                #myCmd = 'echo "' + c +  '" | espeak --stdin -wtest.wav && omxplayer -o local test.wav > /dev/null && rm test.wav'
                 #print(myCmd)
                 os.system(myCmd)
         #for c in word:
