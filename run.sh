364879fa5e7a7e9361238269b186f8f42073b785
robreder <r.reder@gmx.at>
2025-03-10 17:00:13 +0100

    Update run.sh
    Old Raspberry Pi OS-Versionen (before Bullseye) had ALSA without PipeWire → amixer cset numid=3 was working there.
    
    New versions (Bullseye/Bookworm) are using PipeWire or PulseAudio → numid=3 is not working anymore.
        different Pi-Models can have different devices (pactl list sinks short  ... shows the difference).
    
    ⚡ Maybe this Skript has to be adapted then to 
    pactl list sinks short
    
    👉 🚀🎵

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # remember original audio output channel to set it back after running the script
-origAudChan=`amixer cset numid=3 | grep ": values=" | cut -d= -f2`
-analogChan=1
-echo "Original channel is $origAudChan ."
+origAudChan=$(pactl get-default-sink)
+echo "Original channel is $origAudChan."
+
 # set audio output to analog
-amixer cset numid=3 $analogChan
+pactl set-default-sink alsa_output.platform-bcm2835_audio.stereo-fallback
 
 # start IP output on analog audio channel
 python3 sayIp4Addr.py 2>/dev/null
 
 # set back original audio output channel
-amixer cset numid=3 $origAudChan
+pactl set-default-sink "$origAudChan"
